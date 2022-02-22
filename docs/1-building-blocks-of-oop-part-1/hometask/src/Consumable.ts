0652211b3badd97b0f81eaff75f4f6f718da50b6
mkazakov <Mikhail_Kazakov@epam.com>
2022-02-22 03:06:07 +0300

    little fix hometask 1

--- docs/1-building-blocks-of-oop-part-1/hometask/src/Consumable.ts
+++ docs/1-building-blocks-of-oop-part-1/hometask/src/Consumable.ts
@@ -1,6 +1,6 @@
 import { Item } from './Item';
 
-export class Consumable extends Item {
+export abstract class Consumable extends Item {
     private consumed: boolean = false;
     private spoiled: boolean = false;
 
